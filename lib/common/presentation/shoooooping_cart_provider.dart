43557d6e04e02a343ddc07bb5df1db83f1a88626
kosta120 <tsoutsis90@googlemail.com>
2024-02-14 10:43:53 +0100

    a

--- lib/common/presentation/shoooooping_cart_provider.dart
+++ lib/common/presentation/shoooooping_cart_provider.dart
@@ -1,1 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:shoooooping_provider_app/common/domain/product.dart';
 
+class CartProvider extends ChangeNotifier {
+  List<Product> _items = [];
+}
